f1557998ab8437f276ad2b63200cee274e245da9
Dhananjay Hire <coderxdhananjay07@gmail.com>
2025-12-13 19:59:31 +0000

    init: Basic structuring and Connect button

---
+++ src/ranbowKitConfig.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { getDefaultConfig } from "@rainbow-me/rainbowkit"
+import { get } from "http"
+import { anvil, mainnet, zksync } from "wagmi/chains"
+
+export default getDefaultConfig({
+  appName: "TSender",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+  chains: [anvil, zksync, mainnet],
+  ssr: false,
+})
